621811559b8246aeec59d23ab087fd8c918b8291
Ryan Swope <rswope@middlebury.edu>
2018-05-15 15:35:26 -0400

    Cleanup

--- TEST_GAN/classify_image.py
+++ TEST_GAN/classify_image.py
@@ -15,6 +15,8 @@
 
 """Simple image classification with Inception"""
 
+##### MODIFIED FOR PRUNING DATASET FOR GAN ########
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
